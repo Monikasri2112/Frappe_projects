0156434f2116e1be9689a4d27c0849808df1348a
Monika Sri <monikarthik2122@gmail.com>
2026-08-28 05:30:21 +0000

    Add utility API

---
+++ practice_app/util.py
@@ -0,0 +1,30 @@
+import frappe
+from frappe.utils import now
+@frappe.whitelist()
+def get_recent_todos():
+    todos=frappe.get_list(
+        "ToDo",
+        fields=["name","description","owner"],
+        order_by="creation desc",
+        limit_page_length=5
+
+    )
+    records=[]
+
+    for todo in todos:
+        email=frappe.db.get_value(
+            "User",
+            todo.owner,
+            "email"
+        )
+        records.append({
+            "name":todo.name,
+            "description":todo.description,
+            "owner":todo.owner,
+            "email":email
+        })
+
+    return{
+        "timestamp":now(),
+        "records":records
+    }
